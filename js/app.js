a1fbf6fabb1290f0f61d8ac4b349529b80c2f945
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-08 10:44:55 -0700

    some progress on template system

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var projects = [];
+
+function Projects(passedData) {
+  this.title = passedData.title;
+  this.imageUrl = passedData.imageUrl;
+  this.imageAlt = passedData.imageAlt;
+  this.caption = passedData.caption;
+  this.date = passedData.date;
+}
