6b4183ba2c723248dea2c7a080fcff5a41e1fd7f
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-12 16:30:27 -0700

    added relative timestamps and links to demo sites

--- js/app.js
+++ js/app.js
@@ -16,10 +16,59 @@ var dataObject = {
 
 $(function() {
   var theTemplate = $('#projectTemplate').html();
-  console.log(theTemplate);
   var compileThis = Handlebars.compile(theTemplate);
-  console.log(compileThis);
   var inevitableResult = compileThis(dataObject);
-  console.log(inevitableResult);
   $('#projectList').append(inevitableResult);
-});
+} );
+
+function computeRelativeTimestamps() {
+  for (var i = 0; i < dataObject.projectArray.length; i++) {
+    var projectDate = new Date(dataObject.projectArray[i].date);
+    var today = new Date();
+    var timePassed = today - projectDate;
+    dataObject.projectArray[i].ago = millisecondsToStr(timePassed);
+  }
+}
+
+computeRelativeTimestamps();
+
+// Shamelessly adapted from http://stackoverflow.com/questions/8211744/convert-time-interval-given-in-seconds-into-more-human-readable-form
+function millisecondsToStr( milliseconds ) {
+  // TIP: to find current time in milliseconds, use:
+  // var  current_time_milliseconds = new Date().getTime();
+
+  function numberEnding( number ) {
+    return ( number > 1 ) ? 's' : '';
+  }
+
+  var temp = Math.floor( milliseconds / 1000 );
+  var years = Math.floor( temp / 31536000 );
+  if ( years ) {
+    return years + ' year' + numberEnding( years );
+  }
+  var days = Math.floor( ( temp %= 31536000 ) / 86400 );
+  if ( days ) {
+    if (days < 11) {
+      return days + ' day' + numberEnding( days );
+    }
+    var weeks = Math.ceil(days / 7);
+    if (weeks < 4) {
+      return weeks + ' weeks';
+    }
+    var months = Math.floor(weeks / 4);
+    return months + ' month' + numberEnding( months );
+  }
+  var hours = Math.floor( ( temp %= 86400 ) / 3600 );
+  if ( hours ) {
+    return hours + ' hour' + numberEnding( hours );
+  }
+  var minutes = Math.floor( ( temp %= 3600 ) / 60 );
+  if ( minutes ) {
+    return minutes + ' minute' + numberEnding( minutes );
+  }
+  var seconds = temp % 60;
+  if ( seconds ) {
+    return seconds + ' second' + numberEnding( seconds );
+  }
+  return 'less than a second'; //'just now' //or other string you like;
+}
