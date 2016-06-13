8b916be17bcc376a13cdda2e7caa4831b60df101
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-12 19:12:15 -0700

    day 2 changes done

--- js/app.js
+++ js/app.js
@@ -1,74 +1,74 @@
-var dataObject = {
-  projectArray: projects
-};
+// Initialize page
+$(document).ready(function() {
+  // Build object from saved data
+  var data = {
+    projectArray: []
+  };
+  for (var i = 0; i < projects.length; i++) {
+    data.projectArray.push(new Project(projects[i]));
+  }
 
-// function Projects(passedData) {
-//   this.title = passedData.title;
-//   this.imageUrl = passedData.imageUrl;
-//   this.imageAlt = passedData.imageAlt;
-//   this.caption = passedData.caption;
-//   this.date = passedData.date;
-//   this.save = function(){
-//     dataObject.projects.push(this);
-//   }
-//   this.save();
-// }
+  // Go through each object in the array, attach it to the DOM
+  for (var i = 0; i < data.projectArray.length; i++) {
+    data.projectArray[i].toHtml($('#articleTemplate'),$('#projectSection'));
+  }
+});
 
-$(function() {
-  var theTemplate = $('#projectTemplate').html();
-  var compileThis = Handlebars.compile(theTemplate);
-  var inevitableResult = compileThis(dataObject);
-  $('#projectList').append(inevitableResult);
-} );
+// Object constructor
+function Project(passedObject) {
+  // Pull each key:value pair into the new object
+  for (var key in passedObject) {
+    this[key] = passedObject[key];
+  }
+  this.relativeTimestamp;
+}
 
-function computeRelativeTimestamps() {
-  for (var i = 0; i < dataObject.projectArray.length; i++) {
-    var projectDate = new Date(dataObject.projectArray[i].date);
+Project.prototype.toHtml = function(templateElement,appendDiv) {
+  var $tempClone = templateElement.clone();
+  $tempClone.find('h3').text(this.title);
+  $tempClone.find('h5').text(this.date);
+  this.updateRelativeTimestamp();
+  $tempClone.find('h6').text('(' + this.relativeTimestamp + ' ago)');
+  $tempClone.find('#projectUrl').attr('href', this.projectUrl);
+  $tempClone.find('#imageUrl').attr('src', this.imageUrl);
+  $tempClone.find('figcaption').text(this.caption);
+  $tempClone.removeClass('template');
+  appendDiv.append($tempClone);
+}; // End of toHtml()
+
+Project.prototype.updateRelativeTimestamp = function() {
+  if (typeof(this.date) !== 'undefined') {
+    var projectDate = new Date(this.date);
     var today = new Date();
     var timePassed = today - projectDate;
-    dataObject.projectArray[i].ago = millisecondsToStr(timePassed);
-  }
-}
-
-computeRelativeTimestamps();
-
-// Shamelessly adapted from http://stackoverflow.com/questions/8211744/convert-time-interval-given-in-seconds-into-more-human-readable-form
-function millisecondsToStr( milliseconds ) {
-  // TIP: to find current time in milliseconds, use:
-  // var  current_time_milliseconds = new Date().getTime();
-
-  function numberEnding( number ) {
-    return ( number > 1 ) ? 's' : '';
+    this.relativeTimestamp = millisecondsToStr(timePassed);
+  } else {
+    this.relativeTimestamp = 'An unknown amount of time';
   }
 
-  var temp = Math.floor( milliseconds / 1000 );
-  var years = Math.floor( temp / 31536000 );
-  if ( years ) {
-    return years + ' year' + numberEnding( years );
-  }
-  var days = Math.floor( ( temp %= 31536000 ) / 86400 );
-  if ( days ) {
-    if (days < 11) {
-      return days + ' day' + numberEnding( days );
+  // Adapted from http://stackoverflow.com/questions/8211744/convert-time-interval-given-in-seconds-into-more-human-readable-form
+  function millisecondsToStr(milliseconds) {
+    function pluralize(number) {
+      return (number > 1) ? 's' : '';
     }
-    var weeks = Math.ceil(days / 7);
-    if (weeks < 4) {
-      return weeks + ' weeks';
+
+    var seconds = Math.floor(milliseconds / 1000);
+    var years = Math.floor(seconds / 31536000);
+    if (years) {
+      return years + ' year' + pluralize(years);
     }
-    var months = Math.floor(weeks / 4);
-    return months + ' month' + numberEnding( months );
-  }
-  var hours = Math.floor( ( temp %= 86400 ) / 3600 );
-  if ( hours ) {
-    return hours + ' hour' + numberEnding( hours );
-  }
-  var minutes = Math.floor( ( temp %= 3600 ) / 60 );
-  if ( minutes ) {
-    return minutes + ' minute' + numberEnding( minutes );
-  }
-  var seconds = temp % 60;
-  if ( seconds ) {
-    return seconds + ' second' + numberEnding( seconds );
-  }
-  return 'less than a second'; //'just now' //or other string you like;
-}
+    var days = Math.floor((seconds %= 31536000) / 86400);
+    if (days) {
+      if (days < 11) {
+        return days + ' day' + pluralize(days);
+      }
+      var weeks = Math.ceil(days / 7);
+      if (weeks < 4) {
+        return weeks + ' weeks';
+      }
+      var months = Math.floor(weeks / 4);
+      return months + ' month' + pluralize(months);
+    }
+    return 'less than a day'; //'just now' //or other string you like;
+  };
+}; // End of updateRelativeTimestamp()
