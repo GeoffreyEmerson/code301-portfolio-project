e2c397599d18b17c9e72e257826565c1a111c1fa
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-22 07:41:24 -0700

    Added AJAX success/failure notifications and fallback method

--- model/repo-model.js
+++ model/repo-model.js
@@ -8,8 +8,24 @@
       method: 'GET',
       url: 'http://localhost:3000/github/user/repos',
       success: function () {
+        console.log('Proxy AJAX call successful.');
         repos.all = $ajaxResponse.responseJSON;
         if (callback) callback();
+      },
+      error: function() {
+        console.log('Proxy AJAX call has failed. Trying direct request.');
+        var $ajaxResponse = $.ajax({
+          method: 'GET',
+          url: 'https://api.github.com/users/GeoffreyEmerson/repos',
+          success: function () {
+            console.log('Direct AJAX request success.');
+            repos.all = $ajaxResponse.responseJSON;
+            if (callback) callback();
+          },
+          error: function() {
+            console.log('Direct AJAX call has failed. Aborting.');
+          }
+        });
       }
     });
   };
